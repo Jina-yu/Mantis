7d74aa0d9c00cad8e0b334f660316e5f2ce210d6
sidorit <lsy0362@likelion.org>
2021-08-11 19:50:55 +0900

    8월-11일-front-시윤1
    강의상세페이지,
    강의재생페이지(여기에 리뷰작성추가),
    이벤트 목록,
    이벤트 상세페이지,
    이벤트 작성,
    이벤트 수정,
    추가

--- Mentisproject/mentoringapp/urls.py
+++ Mentisproject/mentoringapp/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-
+    path('mentoring', views.mentoring, name='mentoring'),
+    path('event',views.event, name='event'),
+    path('event_detail',views.event_detail, name='event_detail'),    
+    path('event_up',views.event_up, name='event_up'),
+    path('event_re',views.event_re, name='event_re'),
+    path('class1',views.class1, name='class1'),
+    path('class1_play',views.class1_play, name='class1_play'),
 ]
--- Mentisproject/mentoringapp/views.py
+++ Mentisproject/mentoringapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mentoring(request):
+    return render(request, "mentoring.html")
+    
+def event(request):
+    return render(request, "event.html")
+
+def event_detail(request):
+    return render(request, "event_detail1.html")
+
+def event_up(request):
+    return render(request, "event_up.html")
+
+def event_re(request):
+    return render(request, "event_re.html")
+
+def class1(request):
+    return render(request, "class1.html")
+
+def class1_play(request):
+    return render(request, "class1_play.html")
